b9e9ce01d4b1a756ff018d34111a6c191258cdf7
DESKTOP-112MOQR\takki <takkie91@hotmail.com>
2017-12-21 00:03:03 +0100

    add quest and deploy button. add unitpool panel

---
+++ Assets/Scripts/GuildBehaviorScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuildBehaviorScript : MonoBehaviour {
+
+    public int gold;
+    public int maxUnits;
+    public int unitsDeployed;
+
+	// Use this for initialization
+	void Start () {
+        maxUnits = 5;
+        unitsDeployed = 0;
+        gold = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void DeployUnit()
+    {
+        unitsDeployed++;
+    }
+
+    void CancelDeploy()
+    {
+        unitsDeployed--;
+    }
+}

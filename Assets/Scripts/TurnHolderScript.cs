cabaad709668a2eaa36ad5c0e3585ff103040854
vuong pham <vuong.pham@nos.nl>
2017-12-20 21:44:03 +0100

    Add new script

---
+++ Assets/Scripts/TurnHolderScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnHolderScript : MonoBehaviour {
+
+	// List of players.
+	public List<GameObject> players;
+
+	// Indicates which index turn it is.
+	public int turnIndex;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Moves the turnIndex to the next player and returns that player
+	public GameObject nextTurn()
+	{
+		turnIndex++;
+		if (turnIndex >= players.Count){
+			turnIndex = 0;
+		}
+		return players[turnIndex];
+	}
+}
